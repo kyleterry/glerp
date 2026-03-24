e6e331303360533698d23151c6e114cc62b9df59
Kyle Terry <kyle@kyleterry.com>
2026-03-24 11:18:21 -0700

    stdlib/scheme/math: adds pi 'constant'. can definitely be rebound :D

--- stdlib/scheme/math.scm
+++ stdlib/scheme/math.scm
@@ -1,5 +1,5 @@
 ; (scheme math) — mathematical utilities
-(export cube average clamp)
+(export cube average clamp pi)
 
 ;; Returns x raised to the third power.
 (define (cube x)
@@ -13,3 +13,6 @@
 ;; If x < lo, returns lo. If x > hi, returns hi. Otherwise returns x.
 (define (clamp x lo hi)
   (if (< x lo) lo (if (> x hi) hi x)))
+
+;; Returns pi
+(define pi 3.14159265358979323846264338327950288419716939937510582097494459)
